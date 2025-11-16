a0804439fc2e0102bc30c59ee27a5959c411c56b
Manoj Nallusamy <manoj@Manojs-MacBook-Air.local>
2025-11-16 22:20:38 +0530

    refactor site structure for extansibility

--- js/game_manager.js
+++ js/game_manager.js
@@ -153,7 +153,7 @@ GameManager.prototype.fillLegend = function () {
     grid.classList.add('legend-grid');
     cell.classList.add('legend-cell');
     cell.classList.add('cell-' + exp);
-    img.src = "style/img/" + exp + ".jpg";
+    img.src = "/style/img/" + exp + ".jpg";
     img.alt = Localize(exp) + " cupcake in 2048";
     cell.appendChild(img);
     grid.appendChild(cell);
--- js/html_actuator.js
+++ js/html_actuator.js
@@ -69,7 +69,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   inner.classList.add("tile-inner");
   // inner.textContent = tile.value;
   // img.style.width = '100%';
-  img.src = "style/img/" + tile.value + ".jpg";
+  img.src = "/style/img/" + tile.value + ".jpg";
   img.alt = Localize(tile.value) + " cupcake tile";
   inner.appendChild(img);
 
